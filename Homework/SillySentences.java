7757095ca30b8a1e11ad9c78973683730c5ced0a
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-02-09 13:33:33 -0500

    Add initial problem setup

---
+++ Homework/SillySentences.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SillySentences{
+    public static void main(String[] args) {
+        
+        /*
+        4. Write a program that asks the user to enter a favorite color, a favorite food, a favorite
+        animal, and the first name of a friend or relative. The program should then print the
+        following two lines, with the user’s input replacing the items in italics:
+        I had a dream that Name ate a Color Animal
+        and said it tasted like Food!
+        For example, if the user entered blue for the color, hamburger for the food, dog for the
+        animal, and Jake for the person’s name, the output would be
+        I had a dream that Jake ate a blue dog
+        and said it tasted like hamburger!
+        Don’t forget to put the exclamation mark at the end.
+        */
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("INITIAL REQUEST");
+        String input1 = keyboard.nextLine();
+        System.out.println("INITIAL REQUEST");
+        String input2 = keyboard.nextLine();
+        System.out.println("INITIAL REQUEST");
+        String input3 = keyboard.nextLine();
+        System.out.println("INITIAL REQUEST");
+        String input4 = keyboard.nextLine();
+
+        System.out.println("RESULTS: " +
+        "LINE 1\n" +
+        "LINE 2");
+    }
+}
