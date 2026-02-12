66818433240d3026e995f73089080abd3ba235ed
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-02-12 18:18:28 -0500

    Add initial setup for problems six, seven

--- Homework/HomeworkThreeFlowControl.java
+++ Homework/HomeworkThreeFlowControl.java
@@ -6,6 +6,13 @@ public class HomeworkThreeFlowControl{
     public static void main (String [] args) {
         Scanner keyboard = new Scanner(System.in);
 
+        // Problem 5 setup
+        String letter = keyboard.nextLine();
+        System.out.println("gradeValue's result is: " + problemFive(letter));
+    }
+
+
+    public static double problemFive(String letter){
         /*
         5. Write a switch statement to convert a letter of grade into an 
         equivalent numeric value on a four-point scale. Set the value of 
@@ -13,13 +20,6 @@ public class HomeworkThreeFlowControl{
         1.0 for a D, and 0.0 for an F. For any other letter, 
         set the value to 0.0 and display an error message.
         */
-       String letter = keyboard.nextLine();
-       System.out.println("gradeValue's result is: " + problemFour(letter));
-    }
-
-
-    public static double problemFour(String letter){
-        double gradeValue;
 
         switch (letter){
             case "A":
@@ -43,4 +43,26 @@ public class HomeworkThreeFlowControl{
                 return gradeValue;
             }
     }
+
+    public static double problemSix(String letter){
+    /*
+    Consider the previous question but include + or − letter grades. A+ is 4.25, A− is 3.75, B+ is 3.25, B− is 2.75, and so on.
+    1. Why can’t we use one switch statement with no other conditionals to convert these additional letter grades?
+    2. Write a fragment of code that will do the conversion using a multibranch if-else statement.
+    3. Write a fragment of code that will do the conversion using nested switch statements.
+    */
+
+    }
+
+    public static double problemSeven(String letter){
+    /*
+    Write a program to play the rock-paper-scissor game. Each of two users types in 
+    either P, R, or S. The program then announces the winner as well as the basis for 
+    determining the winner: paper covers rock, rock breaks scissors, scissors cuts paper, 
+    or nobody wins. Your program should allow the users to use lowercases as well as uppercase letters.
+    */
+    
+    }
+
+   
 }
