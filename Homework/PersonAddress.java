2da943538544d6b311610174fdc4b9efbc2f2945
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-03-30 15:12:58 -0400

    Initial file set

---
+++ Homework/PersonAddress.java
@@ -0,0 +1,28 @@
+// Consider a class PersonAddress that represents an entry in an address book.
+// Its attributes are
+// • The first name of the person
+// • The last name of the person
+// • The e-mail address of the person
+// • The telephone number of the person
+// It will have methods to
+// • Access each attribute
+// • Change the e-mail address
+// • Change the telephone number
+// • Test whether two instances are equal based solely on name
+// a. Write a method heading for each method.
+// b. Write preconditions and postconditions for each method.
+// c. Write some Java statements that test the class.
+// d. Implement the class.
+
+public class PersonAddress{
+
+    String firstName;
+    String lastName;
+    String emailAddress;
+    int teleNumber;
+
+    // Insert conditions
+    public void sameNameTest(){
+        
+    }
+}
