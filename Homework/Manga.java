22b889171d613ad991b11833aa349fb82b43fc25
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-04-13 16:46:06 -0400

    Add constructor

--- Homework/Manga.java
+++ Homework/Manga.java
@@ -9,4 +9,13 @@ public class Manga{
     //enum for age 
     //date var for date published
     //date var for last update
+
+    public Manga(int id, String title, String[] genre, String audience, String author, String artist){
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.audience = audience;
+        this.author = author;
+        this.artist = artist;
+    }
 }
