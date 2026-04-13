dc13a1811879212e5b51934facf2e26720561471
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-04-13 16:04:01 -0400

    Add test 1.4, change test print for clarity

--- Homework/PersonDemo.java
+++ Homework/PersonDemo.java
@@ -4,29 +4,36 @@ public class PersonDemo {
         Person homunculusOne = new Person("Gamma Theta", 1);
         Person homunculusTwo = new Person("Apyr");
         Person homunculusThree = new Person(444);
+        Person homunculusFour = new Person();
 
         // Test 1.1: Constructor all params 
         if (!homunculusOne.getName().equals("Gamma Theta") ||
         homunculusOne.getAge() != 1) {
-            System.out.println("ERROR 1.1: CONSTRUCTION OR GET-RETURN");
+            System.out.println("ERROR 1.1: CONSTRUCTOR OR GET-RETURN");
         } else {
             System.out.println("PASS 1.1");
         }
 
         // Test 1.2 Constructor name param
         if (!homunculusTwo.getName().equals("Apyr")){
-            System.out.println("ERROR 1.2: CONSTRUCTION OR GET-RETURN");
+            System.out.println("ERROR 1.2: CONSTRUCTOR OR GET-RETURN");
         } else {
             System.out.println("PASS 1.2");
         }
 
         // Test 1.3: Constructor age param
         if (homunculusThree.getAge() != 444){
-            System.out.println("ERROR 1.3: CONSTRUCTION OR GET-RETURN");
+            System.out.println("ERROR 1.3: CONSTRUCTOR OR GET-RETURN");
         } else {
             System.out.println("PASS 1.3");
         }
 
         // Test 1.4: Constructor default param
+        if (!homunculusFour.getName().equals("No name") ||
+        homunculusOne.getAge() != 0) {
+            System.out.println("ERROR 1.4: CONSTRUCTOR OR GET-RETURN");
+        } else {
+            System.out.println("PASS 1.4");
+        }
     }
 }
