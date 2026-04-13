c10bea7cdd428dac6e9e1e85417a87779fd6e2a2
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-04-13 16:01:50 -0400

    Add 1.2, 1.3 tests

--- Homework/PersonDemo.java
+++ Homework/PersonDemo.java
@@ -2,7 +2,9 @@ public class PersonDemo {
     public static void main(String[] args) {
 
         Person homunculusOne = new Person("Gamma Theta", 1);
-        
+        Person homunculusTwo = new Person("Apyr");
+        Person homunculusThree = new Person(444);
+
         // Test 1.1: Constructor all params 
         if (!homunculusOne.getName().equals("Gamma Theta") ||
         homunculusOne.getAge() != 1) {
@@ -10,5 +12,21 @@ public class PersonDemo {
         } else {
             System.out.println("PASS 1.1");
         }
+
+        // Test 1.2 Constructor name param
+        if (!homunculusTwo.getName().equals("Apyr")){
+            System.out.println("ERROR 1.2: CONSTRUCTION OR GET-RETURN");
+        } else {
+            System.out.println("PASS 1.2");
+        }
+
+        // Test 1.3: Constructor age param
+        if (homunculusThree.getAge() != 444){
+            System.out.println("ERROR 1.3: CONSTRUCTION OR GET-RETURN");
+        } else {
+            System.out.println("PASS 1.3");
+        }
+
+        // Test 1.4: Constructor default param
     }
 }
