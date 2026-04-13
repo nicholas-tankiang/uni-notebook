8e795f83aab5f635d69b1769f42a56ccf84c7b36
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-04-13 16:42:30 -0400

    Transfer inst vars to subclass

---
+++ Homework/Manga.java
@@ -0,0 +1,12 @@
+public class Manga{
+    private int id;
+    private String title;
+    private String[] genre;
+    private String audience;
+    private String author;
+    private String artist;
+    //enum for origin
+    //enum for age 
+    //date var for date published
+    //date var for last update
+}
