dedd127d8e73f6a6310d04168060cadc38ac3e6d
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-04-13 14:04:39 -0400

    Add initial files HW6

---
+++ Homework/Person.java
@@ -0,0 +1,45 @@
+// Self-test Question 16 in previous chapter: Give the complete definition of
+// a class called Person that has two instance variables, one for the person’s
+// name and the other for the person’s age. Include accessor methods and
+// mutator methods for each instance variable. Also, include a method that sets
+// both the name and age of a person. There are no other methods.
+// In the previous chapter, Self-Test Question 16 described a class Person to
+// represent a person. The class has instance variables for a person’s name,
+// which is a string, and an integer of age. These variables are name and age,
+// respectively.
+// a. Write a default constructor for Person that sets name to the string "No
+// name yet" and age to zero.
+// b. Write a second constructor for Person that sets name to a given string and
+// age to a given age.
+// c. Write a static method createAdult() for Person that returns a special
+// instance of this class. The instance represents a generic adult and has the
+// name “An adult” and the age 21.
+
+// Repeat Programming Project 2 in Chapter 5 (Same Person Class in
+// Homework 1). This time, add the following four constructor methods: one
+// for each instance variable, one with two parameters for the two instance
+// variables, and a default constructor. Be sure that each constructor sets all of
+// the instance variables. Write a driver program to test each of the methods,
+// including each of the four constructors and at least one true and one false
+// case for each of the test methods.
+// HOMEWORK 3:
+// Complete and fully test the class Person. Include the following additional
+// methods:
+// • getName—returns the name of the person as a string.
+// • getAge—returns the age of the person.
+// • setName(first, last)—sets the name of the person, given a first and last
+// name as strings.
+// • setName(name)—sets the name of the person, given the entire name as one
+// string.
+// • setAge(age)—sets the age of the person.
+// • createToddler—a static method that returns a special instance of the class
+// to represent a toddler. The instance has the name “A toddler” and the age 2.
+// • createPreschooler—a static method that returns a special instance of the
+// class to represent a preschooler. The instance has the name “A preschooler”
+// and the age 5.
+// • createAdolescent—a static method that returns a special instance of the
+// class to represent an adolescent. The instance has the name “An adolescent”
+// and the age 9.
+// • createTeenager—a static method that returns a special instance of the class 
+// to represent a teenager. The instance has the name “A teenager” and the age
+// 15.
