f29fd8cf4e2926c618cc03cf5aa757be67fa6dbf
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-05-11 10:30:44 -0400

    Add github links file

---
+++ Homework/Chapter7Links.java
@@ -0,0 +1,6 @@
+// 1)	Number Above Average: https://github.com/nicholas-tankiang/uni-notebook/blob/main/Homework/NumberAboveAverage.java
+// 2)	Count Families: https://github.com/nicholas-tankiang/uni-notebook/blob/main/Homework/CountFamilies.java
+// 3)	Family Class: https://github.com/nicholas-tankiang/uni-notebook/blob/main/Homework/Family.java
+// 4)	Count Poor: https://github.com/nicholas-tankiang/uni-notebook/blob/main/Homework/CountPoor.java
+// 5)	Flower Counter: https://github.com/nicholas-tankiang/uni-notebook/blob/main/Homework/FlowerCounter.java
+
