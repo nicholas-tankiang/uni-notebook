4cbb37613c7eee6fa701359451466fd459c3f9ad
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2026-05-11 12:14:51 -0400

    Add print all entries, helper func findbyID

--- Homework/MangaDB.java
+++ Homework/MangaDB.java
@@ -8,22 +8,41 @@ public class MangaDB{
     public MangaDB() {
         mangaDatabase = new ArrayList<>();
     }
-    // Assume that a new session has no entries
-    // -case no entries found, prompt creation
 
     //TO DO LIST
     //have menu looping: show menu ->get user choice-> run method-> repeat until exit
     //-menu calls methods
     //-methods do function/return value
 
-    // CRUD:
     // add manga
     public void addManga(Manga m){
         mangaDatabase.add(m);
     }
-    
+
     // read (display) manga entry
     // -display all manga
+    // REQUIRES TOSTRING TO DISPLAY OBJECT TYPE MANGA
+    public void displayAllManga() {
+        if (mangaDatabase.isEmpty()) {
+            System.out.println("No manga in the database.");
+            return;
+        }
+
+        // for each Manga object value in collection mangaDatabase print m
+        for (Manga m : mangaDatabase) {
+            System.out.println(m);
+        }
+    }
+
+    // helper function
+    public Manga findMangaById(int id) {
+        for (Manga m : mangaDatabase) {
+            if (m.getId() == id) {
+                return m;
+            }
+        }
+        return null;
+    }
     // update->modify 
     // -search by id, then update rating/description
     // delete manga
@@ -54,6 +73,10 @@ public class MangaDB{
     public static void main(String[] args) {
         // running loop
     }
+
+    //optional
+    // Assume that a new session has no entries
+    // -case no entries found, prompt creation
 
     // unit test edge cases:
     // user tries to remove something that doesn’t exist
